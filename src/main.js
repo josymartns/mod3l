ecd3d378d29fac6e43a6ea72d4acad2f2b8f6d41
josymartins <25josy25@gmail.com>
2023-09-19 01:25:49 -0300

    Primeiro comentario

---
+++ src/main.js
@@ -0,0 +1,37 @@
+// const leiaEntrada = require("readline");
+// const lendo = leiaEntrada.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+// });
+
+// lendo.question("Qual seu nome? ", function (answer) {
+//     console.log(`Oh, seu nome é ${answer}`);
+// });
+
+
+
+
+// var resultado;
+
+// for(let conta = 1; conta <= 10; conta++){
+    
+// resultado = conta * 2 ; 
+//     console.log('Tabuada do 2 = ' + conta + ' * 2 =' + resultado)
+// }
+
+
+switch(dia)
+{
+    case 1:
+        alert("Hoje é domingo");
+        break;
+    case 2:
+        alert("Hoje é segunda");
+        break;
+    case 3:
+        alert("Hoje é terça");
+        break;
+    default:
+        alert("Hoje não é nem domingo,nem segunda, nem terça");
+        break
+}
