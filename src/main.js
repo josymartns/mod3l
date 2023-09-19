23ec9c7e5c45682c035d7f46ff23f9dd5e3cf6f7
josymartins <25josy25@gmail.com>
2023-09-19 01:27:28 -0300

    Segundo comentario

--- src/main.js
+++ src/main.js
@@ -11,27 +11,10 @@
 
 
 
-// var resultado;
+var resultado;
 
-// for(let conta = 1; conta <= 10; conta++){
+for(let conta = 1; conta <= 10; conta++){
     
-// resultado = conta * 2 ; 
-//     console.log('Tabuada do 2 = ' + conta + ' * 2 =' + resultado)
-// }
-
-
-switch(dia)
-{
-    case 1:
-        alert("Hoje é domingo");
-        break;
-    case 2:
-        alert("Hoje é segunda");
-        break;
-    case 3:
-        alert("Hoje é terça");
-        break;
-    default:
-        alert("Hoje não é nem domingo,nem segunda, nem terça");
-        break
+resultado = conta * 2 ; 
+    console.log('Tabuada do 2 = ' + conta + ' * 2 =' + resultado)
 }
